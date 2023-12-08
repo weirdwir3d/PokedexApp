158cb41481599b84d0fecfdfeb913aceebf84a89
Lisa Aldera <alderalisa1@gmail.com>
2023-12-08 22:08:36 +0100

    added splashscreen and app icon

--- Pokedex/Pages/PokemonDetailPage.swift
+++ Pokedex/Pages/PokemonDetailPage.swift
@@ -1,9 +1,4 @@
-//
-//  PokemonDetailPage.swift
-//  student721009
-//
-//  Created by hohe on 13.10.23.
-//
+
 
 import Foundation
 import SwiftUI
@@ -25,117 +20,115 @@ struct PokemonDetailPage: View {
     var body: some View {
         
         ScrollView{
-                VStack {
-                    HStack {
-                        Text(pokemon.name)
-                            .font(.system(size: 34))
-                            .fontWeight(.bold)
-                            .multilineTextAlignment(.trailing)
-                        
-                        Spacer()
-                        
-                        Text("#" + String(pokemon.id))
-                            .font(.system(size: 30))
-                            .multilineTextAlignment(.leading)
-                    }
-                    .padding()
-                    
-                    
-                    
-                    if let details = detailStore.details {
-                        switch details {
-                        case .success(let details):
-                            VStack {
-                                
-                                HStack{
-                                    ForEach(details.types, id: \.self) { type in
-                                        TypeBadge(type: type.name.capitalized)
-                                    }
-                                }
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .padding(.top, -32)
-                                
-                                
-                                AsyncImage(
-                                    url: pokemon.imageUrl,
-                                    content: { image in
-                                        image
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fill)
-                                            .frame(width: 300, height: 300)
-                                        //                                            .cornerRadius(10)
-                                    },
-                                    placeholder: {
-                                        Color(hex: "#5c5c5c")
-                                    }
-                                )
-                                .toolbar {
-                                    ToolbarItem(placement: .topBarTrailing) {
-                                        Button(action: { favoritesStore.toggle(for: pokemon) }) {
-                                            let isFavorite = favoritesStore.isFavorite(pokemon)
-                                            Image(systemName: isFavorite ? "heart.fill" : "heart")
-                                        }
-                                    }
-                                    
-                                    ToolbarItem(placement: .topBarTrailing) {
-                                            Button(action: {
-                                                let pokemonImg = pokemon.imageUrl
-
-                                                let activityViewController = UIActivityViewController(
-                                                    activityItems: [pokemonImg],
-                                                    applicationActivities: nil
-                                                )
-
-                                                UIApplication.shared.windows.first?.rootViewController?.present(
-                                                    activityViewController,
-                                                    animated: true,
-                                                    completion: nil
-                                                )
-                                            }) {
-                                                Image(systemName: "square.and.arrow.up")
-                                            }
-                                            .tint(Color.black)
-                                        }
-                                    
-                                }
-                                
-                                AboutRow(label: "Name:", value: pokemon.name.capitalized)
-                                AboutRow(label: "ID:", value: String(pokemon.id))
-                                AboutRow(label: "Base:", value: details.baseExp.formatted() + " XP")
-                                AboutRow(label: "Weight:", value: details.weight.formatted() + " kg")
-                                AboutRow(label: "Height:", value: details.height.formatted() + " m")
-                                AboutRow(label: "Types:", value: details.types.map { $0.name.capitalized }.joined(separator: ", "))
-                                AboutRow(label: "Abilities:", value: details.abilities.map { $0.name.capitalized }.joined(separator: ", "))
-                                Text("This text is only here to make the page bigger and show that it is actually scrollable and you would be able to see all details also on smaller phones with smaller screens ")
-                            }
-                            .padding()
-                            .environmentObject(detailStore)
-                        case .failure(let error):
-                            Text("Something went wrong in the Pokemon Detils page: \(error.localizedDescription)")
-                                .padding(16)
-                            Button("Retry", action: {
-                                Task {
-                                    detailStore.details = nil
-                                    await detailStore.fetchDetails(for: pokemon)
-                                }
-                            })
-                            
-                        }
-                    } else {
-                        ProgressView()
-                    }
+            VStack {
+                HStack {
+                    Text(pokemon.name)
+                        .font(.system(size: 34))
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.trailing)
                     
                     Spacer()
+                    
+                    Text("#" + String(pokemon.id))
+                        .font(.system(size: 30))
+                        .multilineTextAlignment(.leading)
+                }
+                .padding()
+                
+                
+                
+                if let details = detailStore.details {
+                    switch details {
+                    case .success(let details):
+                        VStack {
+                            
+                            HStack{
+                                ForEach(details.types, id: \.self) { type in
+                                    TypeBadge(type: type.name.capitalized)
+                                }
+                            }
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.top, -32)
+                            
+                            
+                            AsyncImage(
+                                url: pokemon.imageUrl,
+                                content: { image in
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 300, height: 300)
+                                },
+                                placeholder: {
+                                    Color(hex: "#5c5c5c")
+                                }
+                            )
+                            .toolbar {
+                                ToolbarItem(placement: .topBarTrailing) {
+                                    Button(action: { favoritesStore.toggle(for: pokemon) }) {
+                                        let isFavorite = favoritesStore.isFavorite(pokemon)
+                                        Image(systemName: isFavorite ? "heart.fill" : "heart")
+                                    }
+                                }
+                                
+                                ToolbarItem(placement: .topBarTrailing) {
+                                    Button(action: {
+                                        let pokemonImg = pokemon.imageUrl
+                                        
+                                        let activityViewController = UIActivityViewController(
+                                            activityItems: [pokemonImg],
+                                            applicationActivities: nil
+                                        )
+                                        
+                                        UIApplication.shared.windows.first?.rootViewController?.present(
+                                            activityViewController,
+                                            animated: true,
+                                            completion: nil
+                                        )
+                                    }) {
+                                        Image(systemName: "square.and.arrow.up")
+                                    }
+                                }
+                                
+                            }
+                            
+                            AboutRow(label: "Name:", value: pokemon.name.capitalized)
+                            AboutRow(label: "ID:", value: String(pokemon.id))
+                            AboutRow(label: "Base:", value: details.baseExp.formatted() + " XP")
+                            AboutRow(label: "Weight:", value: details.weight.formatted() + " kg")
+                            AboutRow(label: "Height:", value: details.height.formatted() + " m")
+                            AboutRow(label: "Types:", value: details.types.map { $0.name.capitalized }.joined(separator: ", "))
+                            AboutRow(label: "Abilities:", value: details.abilities.map { $0.name.capitalized }.joined(separator: ", "))
+                            Text("This text is only here to make the page bigger and show that it is actually scrollable and you would be able to see all details also on smaller phones with smaller screens ")
+                        }
+                        .padding()
+                        .environmentObject(detailStore)
+                    case .failure(let error):
+                        Text("Something went wrong in the Pokemon Detils page: \(error.localizedDescription)")
+                            .padding(16)
+                        Button("Retry", action: {
+                            Task {
+                                detailStore.details = nil
+                                await detailStore.fetchDetails(for: pokemon)
+                            }
+                        })
+                        
+                    }
+                } else {
+                    ProgressView()
                 }
                 
+                Spacer()
             }
-            .frame(alignment: .top)
-            .navigationBarTitleDisplayMode(.inline)
-            .task {
-                await detailStore.fetchDetails(for: pokemon)
-            }
+            
         }
-        
+        .frame(alignment: .top)
+        .navigationBarTitleDisplayMode(.inline)
+        .task {
+            await detailStore.fetchDetails(for: pokemon)
+        }
+    }
+    
     
     
     
